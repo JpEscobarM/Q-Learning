d7780b836bbc3cae159070768591d9a55d7e7921
JpEscobarM <jp.escobarmartins@gmail.com>
2025-06-09 15:56:08 -0300

    update main.py
    print QTable

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Mapa:
         self.agente = agente
 
     def resetPosicao(self): #def reset(self)
-        self.agente.pos = [4,3]
+        self.agente.pos = [9,4]
         return self.agente.pos.copy()
 
     def andar(self, direcaoEscolhida): # def act(self, action):
@@ -37,19 +37,15 @@ class Mapa:
         #LOGICA DE LIMITAÇÃO DE PAREDES DO MAPA
 
         if((self.agente.pos[1] < 4) or (self.agente.pos[1] > 7) ) and ( self.agente.pos[0] < 5) :
-            print("#IF 1")
             self.agente.pos[0] = min(4, max(0, self.agente.pos[0]))
             self.agente.pos[1] = min(self.larguraMapa-1, max(0, self.agente.pos[1]))
         elif((5 <= self.agente.pos[0] ) and ( 4 <= self.agente.pos[1] <= 7)):
-            print("#IF 2")
             self.agente.pos[0] = min(self.alturaMapa - 1, max(0, self.agente.pos[0]))
             self.agente.pos[1] = min(7, max(4, self.agente.pos[1]))
         elif(self.agente.pos[0]>=5 and  (7 < self.agente.pos[1] or self.agente.pos[1]<4) ):
-            print("#IF 3")
             self.agente.pos[0] = min(self.alturaMapa-1, max(0, self.agente.pos[0]))
             self.agente.pos[1] = min(7, max(4, self.agente.pos[1]))
         elif (self.agente.pos[0] < 5):
-            print("#IF 4")
             self.agente.pos[0] = min(self.alturaMapa - 1, max(0, self.agente.pos[0]))
             self.agente.pos[1] = min(self.larguraMapa-1, max(0, self.agente.pos[1]))
 
@@ -81,10 +77,37 @@ class Mapa:
             final = True
         return self.agente.pos.copy(), recompensa, final
 
+class QLearning:
+    def __init__(self, epsilon=0.1, alpha=0.1, gamma=0.9, init=0, w=12, h=10, a=4):
+        self.epsilon = epsilon #TAXA DE ALEATORIEDADE
+        self.alpha = alpha #TAXA DE APRENDIZAGEM
+        self.gamma = gamma #FATOR DE DESCONTO CONFORME ATUALIZA ESTADOS ANTERIRES
+        self.larguraMapa = w
+        self.alturaMapa = h
+        self.a = a
+        self.init = init
+        self.criaQTable()
 
-# / def __init__(self, agente, width=12, height=10, tropecoValor = 0):
+    def criaQTable(self):  # reinicia a Qtable com 0,0,0,0 (#up #right, #down #left)
+        #formando uma matriz em "3D"
+        self.qtable = [[[self.init] * self.a for j in range(self.larguraMapa)] for i in range(self.alturaMapa)]
+
+    def printQTable(self):# self.qtable[i][j][X] i,j = posicao no mapa | X = valor da ação escolhida
+        print("Pos   |   Up   |  Right |  Down  |  Left  |")
+        for i in range(self.alturaMapa):
+            for j in range(self.larguraMapa):
+                print("{:>2},{:<2} | {:>6} | {:>6} | {:>6} | {:>6} |".format(
+                    i, j,
+                    self.qtable[i][j][0],
+                    self.qtable[i][j][1],
+                    self.qtable[i][j][2],
+                    self.qtable[i][j][3]
+                ))
 
 agt = Agente()
 mp = Mapa(agt,12,10,0)
 
 mp.resetPosicao()
+
+ql = QLearning()
+ql.printQTable()
